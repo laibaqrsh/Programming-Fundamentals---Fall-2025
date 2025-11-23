fcf5fe4005b86b385dbd9fa66ff0ce6b09d55b8e
Laiba <qlaiba59@gmail.com>
2025-11-23 10:49:41 +0500

    Create pattern printing (rectangle).c

---
+++ Practice/pattern printing (rectangle).c
@@ -0,0 +1,16 @@
+// pattern printing arrow
+#include<stdio.h>
+
+int main()
+{
+	printf("  *\n");
+	printf(" ***\n");
+	printf("*****\n");
+	printf("  *\n");
+	printf("  *\n");
+	printf("  *\n");
+	printf("  *\n");
+	
+	
+	return 0;	
+}
